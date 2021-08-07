62059a4b8aef3a997374bb5207c1c9ba76dddbfb
Yejin6911 <cdjin6911@gmail.com>
2021-08-07 10:13:20 +0900

    0731 실습

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,18 +1,12 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.MemberService;
-import entity.Hello;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.util.HashMap;
-import java.util.Map;
 
 //어노테이션: 컴포넌트임을 알려줌
 @Controller
--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,10 +1,9 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.MemberService;
-import entity.Hello;
+import com.example.demo.entity.Hello;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
